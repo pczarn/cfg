cb449cb70bf2a90ab44469465cf515faa5a43376
Piotr Czarnecki <pioczarn@gmail.com>
2024-07-10 10:41:13 +0200

    Run rustfmt

--- cfg/src/lib.rs
+++ cfg/src/lib.rs
@@ -1,7 +1,7 @@
-pub use cfg_grammar::*;
 pub use cfg_classify as classify;
 pub use cfg_earley as earley;
 pub use cfg_generate as generate;
+pub use cfg_grammar::*;
 pub use cfg_predict as predict;
 pub use cfg_sequence as sequence;
 pub use cfg_symbol::SymbolSource;
